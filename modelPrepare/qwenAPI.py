d2acc9825879bbcdd80e914bd4ca1c5d04fc5707
liujingsong111@126.com <liujingsong111@126.com>
2025-06-03 18:35:40 +0800

    stream

--- modelPrepare/qwenAPI.py
+++ modelPrepare/qwenAPI.py
@@ -1,5 +1,5 @@
 from config import config
-from langchain_community.llms import Tongyi
+from langchain_community.chat_models.tongyi import ChatTongyi
 import os
 # qwen-turbo,qwen-plus,qwen-max,qwen-max-longcontext
 # https://help.aliyun.com/zh/dashscope/developer-reference/model-introduction?spm=a2c4g.11186623.0.0.1568140bOMYZzf
@@ -10,10 +10,11 @@ os.environ["DASHSCOPE_API_KEY"] = config.qwen_api_key
 def QwenPlus(model_name="qwen-plus",temperature=0, max_tokens=2000,top_p=0.9):
     text_gen_params = {
         'temperature': temperature,
-        #'max_tokens': max_tokens,
-        #'top_p':top_p
+        'max_tokens': max_tokens,
+        'top_p':top_p
     }
-    llm = Tongyi(model_name=model_name,
-                 #dashscope_api_key=config.qwen_api_key,
+    llm = ChatTongyi(model_name=model_name,
+                 #dashscope_api_key,=config.qwen_api_key,
+                 streaming=True,
                  model_kwargs=text_gen_params)
     return llm
