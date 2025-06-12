08569716a08f76a0c8cd94a3d647c68ef4573ac9
liujingsong111@126.com <liujingsong111@126.com>
2025-06-12 11:48:08 +0800

    kbot3

--- autoStart.sh
+++
@@ -1,2 +0,0 @@
-pushd /home/ubuntu/kbot
-/home/ubuntu/miniconda/envs/kbot/bin/python main.py --port 443
--- test/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-import array
-emebdding=[11,.2]
-vector_data_64 = array.array('d', emebdding)
-print(vector_data_64[1])
