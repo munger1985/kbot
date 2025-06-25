b2e22647185e0a9b7e7b1251ce32ae60b722a7fe
JustinZou <zouzhongfan@gmail.com>
2025-06-25 16:49:00 +0800

    update by justin at 20250625 表结构变更

--- backend/dao/entities/kbot_vecdb_conn_info.py
+++ backend/dao/entities/kbot_vecdb_conn_info.py
@@ -6,16 +6,17 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from backend.core.database.oracle import Base
 
-class KBotDbConnInfo(Base):
-    """KBOT_DB_CONN_INFO table model."""
+class KBotVecDbConnInfo(Base):
+    """KBOT_VECDB_CONN_INFO table model."""
     
-    __tablename__ = "KBOT_DB_CONN_INFO"
+    __tablename__ = "KBOT_VECDB_CONN_INFO"
     
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     app_id: Mapped[int] = mapped_column(BigInteger)
     db_display_name: Mapped[str] = mapped_column(String(256))
     db_type: Mapped[str] = mapped_column(String(20))
     db_conn_str: Mapped[dict] = mapped_column(JSON)
+    status: Mapped[str] = mapped_column(String(1))
     descs: Mapped[Optional[str]] = mapped_column(String(512))
     created_by: Mapped[str] = mapped_column(String(512))
     created_time: Mapped[datetime] = mapped_column(DateTime)
