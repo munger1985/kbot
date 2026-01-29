805f31d087306d0fa27c4b8616a094f6916bb9f9
chrisguy1031 <chrisgu1031@live.com>
2026-01-29 15:41:02 +0800

    fix: 将OCR引擎从easyocr改为tesseract

--- services/kb/kb_file_operator.py
+++ services/kb/kb_file_operator.py
@@ -223,7 +223,7 @@ class KBFileOperator:
                 file_version = fileparam["file_version"],
                 is_overwrite = fileparam["is_overwrite"],
                 security_level = kb_entity.security_level or 1,
-                chunk_parser = json.dumps({"do_ocr": True, "overlap": 50, "use_vlm": True, "vlm_model": 68, "chunk_size": 512, "ocr_engine": "easyocr", "vlm_prompt": prompt, "images_scale": 2.0, "min_chunk_len": 10, "generate_picture_images": True}, cls=DecimalEncoder),
+                chunk_parser = json.dumps({"do_ocr": True, "overlap": 50, "use_vlm": True, "vlm_model": 68, "chunk_size": 512, "ocr_engine": "tesseract", "vlm_prompt": prompt, "images_scale": 2.0, "min_chunk_len": 10, "generate_picture_images": True}, cls=DecimalEncoder),
                 enable_summary = kb_entity.enable_summary,
                 is_img2txt = kb_entity.is_img2txt,
                 is_table_head_fill = kb_entity.is_table_head_fill,
