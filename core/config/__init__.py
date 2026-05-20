3cc94086e224d2bcb79e985c4bcfe1b3b9fe5fe3
chrisguy1031 <chrisgu1031@live.com>
2026-05-20 10:49:10 +0800

    refactor: 添加配置模块初始化文件

---
+++ core/config/__init__.py
@@ -0,0 +1,25 @@
+from .settings import (
+    get_settings,
+    get_app_config,
+    get_embed_config,
+    get_oracle_config,
+    get_llm_config,
+    get_prompt_config,
+    get_reranker_config,
+    get_sqlalchemy_config,
+    get_vlm_config,
+    get_parser_config
+)
+
+__all__ = [
+    "get_settings",
+    "get_app_config",
+    "get_embed_config",
+    "get_oracle_config",
+    "get_llm_config",
+    "get_prompt_config",
+    "get_reranker_config",
+    "get_sqlalchemy_config",
+    "get_vlm_config",
+    "get_parser_config"
+]
